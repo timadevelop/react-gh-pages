8e38fd9fe526e41aa5db33f357c4f85debb21b66
timadevelop <timadevelop@gmail.com>
2018-10-19 18:09:36 +0300

    new particles config & class rename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
             <div>
                 <Background />
                 <div className="App">
-                    <header className="App-header">
+                    <header className="App-content">
                         <img src={logo} className="App-logo" alt="logo" />
                         <p>
                             Sample gh-page.
